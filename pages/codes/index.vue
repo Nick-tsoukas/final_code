30c899753881223237a511867778b4dfe2bd06d1
jasenmichael <jasen@jasenmichael.com>
2021-07-25 21:06:47 -0500

    chore: add refresh button and refactor using nuxt fetch

--- pages/codes/index.vue
+++ pages/codes/index.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="pl-28 pt-10">
-    <pre>{{ codes }}</pre>
+    <button
+      class="py-2 px-4 border border-2 border-gray-300"
+      @click="refreshQrCodes"
+    >
+      refresh codes
+    </button>
+    <pre v-if="$strapi.user.qrCodes.length" class="mt-2"
+      >{{ $strapi.user.qrCodes }}
+    </pre>
     <!-- <DashAllCodes class="flex-grow" :codes="codes" /> -->
   </div>
 </template>
@@ -9,22 +17,27 @@
 export default {
   middleware: "auth",
   layout: "dash",
-  data() {
-    return {
-      codes: []
-    };
-  },
-  computed: {
-    user() {
-      return this.$strapi.user;
-    }
-  },
   async mounted() {
-    // return (this.codes = this.$strapi.user.qrCodes);
-    const data = await this.$strapi.find("qrs", {
-      users_permissions_user: this.$strapi.user.id
-    });
-    this.codes = data;
-  }
+    this.$fetch();
+  },
+  async fetch() {
+    this.$strapi.user.qrCodes = await this.$strapi
+      .find("qrs", {
+        users_permissions_user: this.$strapi.user.id,
+      })
+      .then((qrs) => {
+        return qrs.map((qrCode) => {
+          return qrCode;
+        });
+      });
+  },
+  methods: {
+    refreshQrCodes() {
+      this.$strapi.user.qrCodes = [];
+      setTimeout(async () => {
+        this.$fetch();
+      }, 800);
+    },
+  },
 };
 </script>
