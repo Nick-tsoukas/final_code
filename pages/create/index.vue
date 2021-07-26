d48b55fdddaf59b497af736f6f8515a857fba243
jasenmichael <jasen@jasenmichael.com>
2021-07-25 21:08:12 -0500

    fix: refactor get qr image, and save to strapi media library

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -1,48 +1,52 @@
 <template>
-  <div class="pl-28 p-20 lg:flex lg:flex-row">
-    <div class="w-1/2 flex flex-col ">
-      <!-- <pre>{{ qrImageUrl }}</pre> -->
-
-      <FormulateForm @submit="createCodeTwo" v-model="formValues">
-        <!-- takes the metadata for the code for the code -->
-        <FormulateInput
-          name="title"
-          label="Title"
-          validation="required"
-          label-class=" text-md spartan font-medium"
-          input-class="border-2 border-gray-300 rounded-lg w-72 lg:w-96 h-14 pl-6 "
-          placeholder="google.com"
-        />
-        <FormulateInput
-          name="url"
-          label="Enter the website address to link to your qr code"
-          validation="required"
-          label-class=" text-md spartan font-medium"
-          input-class="border-2 border-gray-300 rounded-lg w-72 lg:w-96 h-14 pl-6 "
-          placeholder="google.com"
-        />
-
-        <FormulateInput
-          id="btn"
-          wrapper-class="flex flex-row  mt-6"
-          class="color: green;"
-          type="submit"
-          label="Create Code"
-        />
-      </FormulateForm>
+  <div class="pl-28 p-20">
+    <div class="lg:flex lg:flex-row">
+      <div class="w-1/2 flex flex-col">
+        <FormulateForm @submit="create" v-model="formValues">
+          <FormulateInput
+            name="title"
+            label="Title"
+            validation="required"
+            label-class=" text-md spartan font-medium"
+            input-class="border-2 border-gray-300 rounded-lg w-72 lg:w-96 h-14 pl-6 "
+            placeholder="google.com"
+          />
+          <FormulateInput
+            name="url"
+            label="Enter the website address to link to your qr code"
+            validation="required"
+            label-class=" text-md spartan font-medium"
+            input-class="border-2 border-gray-300 rounded-lg w-72 lg:w-96 h-14 pl-6 "
+            placeholder="google.com"
+          />
+          <FormulateInput
+            id="btn"
+            wrapper-class="flex flex-row  mt-6"
+            class="color: green;"
+            type="submit"
+            label="Create Code"
+          />
+        </FormulateForm>
+      </div>
+      <section>
+        <div class="w-48 h-48 lg:w-64 lg:h-64">
+          <img
+            v-if="qrImageUrl"
+            class="object-contain h-full w-full"
+            :src="qrImageUrl"
+            alt=""
+          />
+          <div v-else class="h-full w-full bg-gray-50"></div>
+        </div>
+        <button
+          @click="save"
+          class="px-6 py-2 mt-4 text-custom-black bg-custom-blue"
+        >
+          Save
+        </button>
+      </section>
     </div>
-    <section>
-      <img
-        v-if="qrImageUrl"
-        class="object-contain h-full w-full"
-        :src="qrImageUrl"
-        alt=""
-      />
-
-      <button @click="save" class="px-6 py-2 text-custom-black bg-custom-blue">
-        Save
-      </button>
-    </section>
+    <div v-if="errorMsg" class="pt-8">{{ errorMsg }}</div>
   </div>
 </template>
 
@@ -57,78 +61,47 @@ export default {
       code: [],
       testing: null,
       base: null,
-      qrImageUrl: null
+      qrImageUrl: null,
+      qrBlob: null,
+      errorMsg: null,
+      type: "png",
     };
   },
-  async mounted() {
-    // return (this.codes = this.$strapi.user.qrCodes);
-    const data = await this.$strapi.find("qrs", {
-      users_permissions_user: this.$strapi.user.id
-    });
-    this.codes = data;
-  },
-  computed: {
-    codeData() {
-      return this.formValues;
-    },
-    user() {
-      return this.$strapi.user;
-    }
-  },
-  methods: {
-    async uploadByUrl(url) {
-      fetch(url)
-        .then(response => response.blob())
-        .then(function(myBlob) {
-          const formData = new FormData();
-          formData.append("files", myBlob);
-          console.log(formData);
-          fetch("http://localhost:1337/upload", {
-            method: "POST",
-            headers: {
-              Authorization: "Bearer " // <- Don't forget Authorization header if you are using it.
-            },
-            body: formData
-          })
-            .then(response => {
-              const result = response.json();
-              console.log("result", result);
-            })
-            .catch(function(err) {
-              console.log("error:");
-              console.log(err);
-            });
-        });
-    },
-    save: async function() {
-      if (process.client) {
-        const myBlob = await fetch("https:" + this.qrImageUrl, {
-          mode: "no-cors"
-        }).then(response => response.blob());
-        // blob empty
-        console.log(myBlob);
 
+  methods: {
+    async save() {
+      if (process.client) {
+        // upload qr image to strapi, and get the upload id'
         const formData = new FormData();
-        formData.append("files", myBlob);
-        // below broken
-        const qrId = await this.$strapi.create("upload", { body: formData });
-        console.log(qrId);
-        const qr = await this.$strapi.create("qrs", {
-          ...this.formValues,
-          imgUrl: this.qrImageUrl,
-          qrMediaLibraryImage: { id: 3 },
-          users_permissions_user: this.$strapi.user.id
-        });
-        // console.log("creating: ", qr);
+        const label = this.formValues.title.replace(/ /g, "-");
+        formData.append("files", this.qrBlob, label + "." + this.type);
+        const qrId = (await this.$strapi.create("upload", formData))[0].id;
+
+        // create the qr object with the qr image uploaded
+        const qr = await this.$strapi
+          .create("qrs", {
+            ...this.formValues,
+            qrMediaLibraryImage: { id: qrId },
+            users_permissions_user: this.$strapi.user.id,
+          })
+          .catch((err) => {
+            this.errorMsg = "ERROR: " + err;
+          });
+        if (qr && qr.id) {
+          this.$router.push(`/codes/${qr.id}`);
+        }
       }
     },
 
-    async createCodeTwo() {
-      this.$axios.setHeader("x-rapidapi-key", process.env.API_KEY);
-      this.$axios.setHeader("x-rapidapi-host", "qrcode-monkey.p.rapidapi.com");
-
-      this.qrImageUrl = await this.$axios
-        .$post("https://qrcode-monkey.p.rapidapi.com/qr/transparent", {
+    async create() {
+      await fetch("https://qrcode-monkey.p.rapidapi.com/qr/transparent", {
+        method: "POST",
+        headers: {
+          "x-rapidapi-key": process.env.API_KEY,
+          "x-rapidapi-host": "qrcode-monkey.p.rapidapi.com",
+          Accept: `image/${this.type}`,
+        },
+        body: JSON.stringify({
           data: this.formValues.url,
           config: {
             body: "rounded-pointed",
@@ -152,17 +125,22 @@ export default {
             gradientColor2: "#25492F",
             gradientType: "radial",
             gradientOnEyes: false,
-            logo: ""
+            logo: "",
           },
-          download: true,
-          file: "png"
+          download: false,
+          file: this.type,
+        }),
+      })
+        .then((data) => {
+          return data.blob();
         })
-        .then(response => {
-          // console.log(response);
-          return response.imageUrl;
+        .then((result) => {
+          this.qrBlob = new Blob([result], { type: result.type || this.type });
+          this.qrImageUrl = window.URL.createObjectURL(this.qrBlob);
         });
-    }
-  }
+        // this.save()
+    },
+  },
 };
 </script>
 
